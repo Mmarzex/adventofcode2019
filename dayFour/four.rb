d0ed5f592aad05ef6da7d1fee3211fab9e552cfe
Max Marze <max@marze.io>
2019-12-08 22:30:56 -0500

    Finish Day Four

--- dayFour/four.rb
+++ dayFour/four.rb
@@ -1,37 +1,38 @@
 class Integer
-    def digits(base: 10)
-      quotient, remainder = divmod(base)
-      quotient == 0 ? [remainder] : [*quotient.digits(base: base), remainder]
-    end
+  def digits(base: 10)
+    quotient, remainder = divmod(base)
+    quotient == 0 ? [remainder] : [*quotient.digits(base: base), remainder]
+  end
 end
 
 input_lower = 245182
 input_upper = 790572
 
-def is_valid_password?(input, part_two=false)
-    digits = input.digits
-    prev = digits[0]
-    is_double = false
-    is_asc = true
+def is_valid_password?(input, part_two = false)
+  digits = input.digits
+  prev = digits[0]
+  is_double = false
+  is_asc = true
 
-    1.upto(digits.count - 1).each do |v|
-        new_dig = digits[v]
-        if part_two
-            is_double = true if prev == new_dig && prev != digits[v - 2] && new_dig != digits[v + 1]
-        else
-            is_double = true if new_dig == prev
-        end
-        is_asc = false if new_dig < prev
-        prev = new_dig
+  1.upto(digits.count - 1).each do |v|
+    new_dig = digits[v]
+    if part_two
+      is_double = true if prev == new_dig && prev != digits[v - 2] && new_dig != digits[v + 1]
+    else
+      is_double = true if new_dig == prev
     end
-    is_double && is_asc
+    is_asc = false if new_dig < prev
+    prev = new_dig
+  end
+  is_double && is_asc
 end
+
 p1_result = (input_lower..input_upper).filter do |x|
-    is_valid_password?(x)
+  is_valid_password?(x)
 end
 p2_result = (input_lower..input_upper).filter do |x|
-    is_valid_password?(x, true)
+  is_valid_password?(x, true)
 end
-puts "#{p1_result.count}"
-puts "#{p2_result.count}"
+puts "p1: #{p1_result.count}"
+puts "p2: #{p2_result.count}"
 
